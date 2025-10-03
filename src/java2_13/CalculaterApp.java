643ab10dfd5329751a32d383d38f90cccd59ca36
Nichika Koizumi <bsi25014@uhe.ac.jp>
2025-10-03 13:52:28 +0900

    四則演算機能の追加

--- src/java2_13/CalculaterApp.java
+++ src/java2_13/CalculaterApp.java
@@ -8,27 +8,67 @@ public class CalculaterApp {
 		Culculator calc = new Culculator();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("実行する計算を選んでください");
-		System.out.println("1平方数");
-		System.out.println("2階乗");
-		System.out.println("3終了");
+		System.out.println("1足し算");
+		System.out.println("2引き算");
+		System.out.println("3掛け算");
+		System.out.println("4割り算");
+		System.out.println("5平方数");
+		System.out.println("6階乗");
+		System.out.println("7終了");
 		int input = scanner.nextInt();
 		
+		
+		
 		if(input == 1) {
-		System.out.println("数字を一つ入力してください");
-		double argument = scanner.nextDouble();
-		calc.squareNumber(argument);
+		System.out.println("1つ目の数字を入力してください");
+		double argument1 = scanner.nextDouble();
+		System.out.println("2つ目の数字を入力してください");
+		double argument2 = scanner.nextDouble();
+		calc.add(argument1,argument2);
 		}
 		if(input == 2) {
+			System.out.println("1つ目の数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("2つ目の数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			calc.sub(argument1,argument2);
+			}
+		if(input == 3) {
+			System.out.println("1つ目の数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("2つ目の数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			calc.mul(argument1,argument2);
+			}
+		if(input == 4) {
+			System.out.println("1つ目の数字を入力してください");
+			double argument1 = scanner.nextDouble();
+			System.out.println("2つ目の数字を入力してください");
+			double argument2 = scanner.nextDouble();
+			calc.div(argument1,argument2);
+			}
+		if(input == 5) {
+			System.out.println("数字を1つ入力してください");
+			double argument = scanner.nextDouble();
+			calc.squareNumber(argument);
+		}
+		if(input == 6) {
 			System.out.println("数字を一つ入力してください");
 			int num = scanner.nextInt();
 			calc.factorial(num);
 		}
-		if(input ==3) {
+		if(input ==7) {
 			System.out.println("プログラムを終了します");
 			scanner.close();
 			return;
-		}
+		
+		}else {
+		System.out.println("1から7の数字を入力してください");
 		scanner.close();
+		return;
+		
+		}
+		
 	}
 
 }
--- src/java2_13/Culculator.java
+++ src/java2_13/Culculator.java
@@ -15,4 +15,20 @@ public class Culculator {
 			System.out.println(answer);
 		
 	}
+	void add(double decimalAdd1,double decimalAdd2) {
+		double answerAdd = decimalAdd1 + decimalAdd2;
+		System.out.println(answerAdd);
+	}
+	void sub(double decimalSub1,double decimalSub2) {
+		double answerSub = decimalSub1 - decimalSub2;
+		System.out.println(answerSub);
+	}
+	void mul(double decimalMul1,double decimalMul2) {
+		double answerMul = decimalMul1 * decimalMul2;
+		System.out.println(answerMul);
+	}
+	void div(double decimalDiv1,double decimalDiv2) {
+		double answerDiv = decimalDiv1 / decimalDiv2;
+		System.out.println(answerDiv);
+	}
 }
